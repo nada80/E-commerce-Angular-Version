256872b58c7620c5aab5b8155ca3d8a2baebcd7b
israa-ahmed-nasr <76089545+israa-ahmed-nasr@users.noreply.github.com>
2021-05-15 00:29:31 +0200

    added some changes from old project version

--- FurnitureWebsite/src/app/app-routing.module.ts
+++ FurnitureWebsite/src/app/app-routing.module.ts
@@ -7,15 +7,23 @@ import { ProductOfCategoryComponent } from './Pages/product-of-category/product-
 import { ProductComponent } from './Pages/product/product.component';
 import { RegisterComponent } from './Pages/register/register.component';
 import { ShopComponent } from './Pages/shop/shop.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductsCategoriesComponent } from './products-categories/products-categories.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},  
+ {path:'',redirectTo:'/home',pathMatch:'full'}, 
   {path: 'home', component: HomeComponent},
   {path: 'shop', component: ShopComponent},
+  {path:'products',component:ProductsComponent},
   {path: 'product', component: ProductComponent},
+ {path:'cart',component:ShoppingCartComponent},
   {path: 'login', component: LoginComponent},  
   {path: 'register', component: RegisterComponent},
   {path: 'category/:id', component: ProductOfCategoryComponent},
+{path:'productsCategories', component:ProductsCategoriesComponent},
+  { path: '404', component: PageNotFoundComponent}
   
 ];
 
